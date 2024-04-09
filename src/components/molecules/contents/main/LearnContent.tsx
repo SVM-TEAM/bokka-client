bd4f9aa76a572137e18b90777f060ec30769c864
motojate <motojate@naver.com>
2024-04-09 23:10:46 +0900

    [FEAT] 메인 페이지 [RV] - 컨텐츠 푸터 고정

--- src/components/molecules/contents/main/LearnContent.tsx
+++ src/components/molecules/contents/main/LearnContent.tsx
@@ -15,75 +15,113 @@ import CommonTooltip from 'src/components/atoms/tooltips/CommonTooltip';
 
 const LearnContent = () => {
   return (
-    <Card
-      sx={{
-        height: 'auto',
-        overflow: 'visible',
-        mt: 3,
-        backgroundColor: '#EAEAEA',
-        borderRadius: '15px',
-      }}
-    >
-      <CardHeader
-        title={
-          <Box sx={{ width: '100%' }}>
-            <LinearBar progress={50} />
-          </Box>
-        }
-      />
-      <Divider variant="middle" />
-      <CardMedia
-        component="img"
-        image="https://i1.sndcdn.com/avatars-g8wTS6RFcvm07Ein-Gru6Lg-t240x240.jpg"
-        alt="캐릭터 이미지"
+    <>
+      <Card
         sx={{
-          width: '50%',
-          margin: 'auto',
+          height: 'auto',
+          overflow: 'visible',
           mt: 3,
-          borderRadius: '50%',
-          border: '2px solid #000',
+          backgroundColor: '#EAEAEA',
+          borderRadius: '15px',
         }}
-      />
-      <CardContent>
+      >
+        <CardHeader title={<LinearBar progress={50} />} />
+        <Divider variant="middle" />
+        <CardMedia
+          component="img"
+          image="https://i1.sndcdn.com/avatars-g8wTS6RFcvm07Ein-Gru6Lg-t240x240.jpg"
+          alt="캐릭터 이미지"
+          sx={{
+            width: '40%',
+            margin: 'auto',
+            mt: 3,
+            borderRadius: '50%',
+            border: '2px solid #000',
+          }}
+        />
+        <Divider variant="middle" sx={{ mt: 3 }} />
+        <CardContent>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <Typography variant="body1">레벨 : 1</Typography>
+            <CommonTooltip title="오늘의 출석">
+              <Avatar
+                sx={{
+                  background:
+                    'linear-gradient(45deg, #FFC0CB, #FFD700, #FFA500)',
+                }}
+              >
+                <StarIcon />
+              </Avatar>
+            </CommonTooltip>
+          </Box>
+        </CardContent>
         <Box
           sx={{
             display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'center',
+            justifyContent: 'center',
+            mt: -2,
           }}
         >
-          <Typography variant="body1">레벨 : 1</Typography>
-          <CommonTooltip title="오늘의 출석">
-            <Avatar
-              sx={{
-                background: 'linear-gradient(45deg, #FFC0CB, #FFD700, #FFA500)',
-              }}
-            >
-              <StarIcon />
-            </Avatar>
-          </CommonTooltip>
+          <Button
+            variant="contained"
+            sx={{
+              fontSize: 'large',
+              borderRadius: '15px',
+              transform: 'translateY(50%)',
+              width: '60%',
+            }}
+          >
+            학습하기
+          </Button>
         </Box>
-      </CardContent>
-      <Box
+      </Card>
+      <Card
         sx={{
+          height: 'auto',
+          mt: 3,
+          backgroundColor: '#EAEAEA',
+          borderRadius: '15px',
           display: 'flex',
-          justifyContent: 'center',
-          mt: -2,
+          flexDirection: 'column', // 이를 통해 CardContent가 flex-grow를 사용하여 남은 공간을 채울 수 있음
         }}
       >
-        <Button
-          variant="contained"
+        <CardHeader title="오늘의 학습률" />
+        <Divider variant="middle" />
+        <CardContent
           sx={{
-            fontSize: 'large',
-            borderRadius: '15px',
-            transform: 'translateY(50%)',
-            width: '60%',
+            overflowY: 'auto', // 내용이 세로로 넘칠 경우 스크롤바 생성
+            flexGrow: 1, // CardContent가 카드 내에서 가능한 많은 공간을 차지하도록 함
           }}
         >
-          학습하기
-        </Button>
-      </Box>
-    </Card>
+          <LinearBar progress={50} />
+          <LinearBar progress={50} /> <LinearBar progress={50} />{' '}
+          <LinearBar progress={50} />
+          <LinearBar progress={50} /> <LinearBar progress={50} />{' '}
+          <LinearBar progress={50} /> <LinearBar progress={50} />
+          <LinearBar progress={50} />
+          <LinearBar progress={50} /> <LinearBar progress={50} />{' '}
+          <LinearBar progress={50} />
+          <LinearBar progress={50} /> <LinearBar progress={50} />{' '}
+          <LinearBar progress={50} /> <LinearBar progress={50} />
+          <LinearBar progress={50} />
+          <LinearBar progress={50} /> <LinearBar progress={50} />{' '}
+          <LinearBar progress={50} />
+          <LinearBar progress={50} /> <LinearBar progress={50} />{' '}
+          <LinearBar progress={50} /> <LinearBar progress={50} />
+          <LinearBar progress={50} />
+          <LinearBar progress={50} /> <LinearBar progress={50} />{' '}
+          <LinearBar progress={50} />
+          <LinearBar progress={50} /> <LinearBar progress={50} />{' '}
+          <LinearBar progress={50} /> <LinearBar progress={50} />
+        </CardContent>
+      </Card>
+    </>
   );
 };
 
